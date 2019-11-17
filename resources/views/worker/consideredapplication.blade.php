1c953844d52770e274ae7e9b976339f73f5e6578
Bartek <bartek12312@o2.pl>
2019-11-18 00:32:37 +0100

    Kierunki i wydzialy Składanie karty

--- resources/views/worker/consideredapplication.blade.php
+++ resources/views/worker/consideredapplication.blade.php
@@ -44,7 +44,7 @@
         <th>Imię</th>
         <th>Nazwisko</th>
         <th>WPIS Biblioteka</th>
-        <th>Anuluj</th>
+        <th>Anuluj wpissssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss</th>
         
     </tr>
     </thead>
