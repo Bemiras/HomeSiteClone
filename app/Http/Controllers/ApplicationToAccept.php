0e6947bc9851c819eb6cb221574bd593851531d4
kamildys <kamildys79@gmail.com>
2019-11-18 01:55:49 +0100

    Podania o zmiane danych
    Wyswietlanie wydzialu i kierunku ( podanie nie zmienia e-maila) BLAD!!!

--- app/Http/Controllers/ApplicationToAccept.php
+++ app/Http/Controllers/ApplicationToAccept.php
@@ -6,13 +6,42 @@ use App\ApplicationForChangingData;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class applicationToAccept extends Controller
 {
     public function index(){
-        $dataToChange = ApplicationForChangingData::all();
-        $userData = User::all();
-        return view ('ApplicationToAccept',["dataToChange" => $dataToChange, "userData" => $userData]);
+        /*
+        //$dataToChange = ApplicationForChangingData::all();
+
+        $dataToChange = DB::table('application_for_changing_datas')
+            ->join('departments', 'departments.id', '=', 'application_for_changing_datas.department')
+            ->join('directions', 'directions.id', '=', 'application_for_changing_datas.direction')
+            ->select('application_for_changing_datas.*','departments.name AS name_department','directions.name AS name_direction')
+            ->get();
+
+        //$userData = User::all();
+
+        $userData = DB::table('users')
+            ->join('departments', 'departments.id', '=', 'users.department')
+            ->join('directions', 'directions.id', '=', 'users.direction')
+            ->select('users.*','departments.name AS name_department','directions.name AS name_direction')
+            ->get();
+*/
+
+        $dataBase = DB::table('application_for_changing_datas')
+            ->join('departments', 'departments.id', '=', 'application_for_changing_datas.department')
+            ->join('directions', 'directions.id', '=', 'application_for_changing_datas.direction')
+            ->join('users', 'users.id', '=', 'application_for_changing_datas.user_id')
+            ->select('application_for_changing_datas.*', 'departments.name AS name_department','directions.name AS name_direction',
+            'users.id AS id_user_users', 'users.name AS old_name', 'users.lastname AS old_lastname', 'users.typestudy AS old_typestudy',
+                'users.levelstudy AS old_levelstudy', 'users.department AS old_department', 'users.direction AS old_direction',
+                'users.specialization AS old_specialization','users.role AS role'
+            )
+            ->get();
+
+        return view ('ApplicationToAccept',["dataBase" => $dataBase]);
+       // return view ('ApplicationToAccept',["dataToChange" => $dataToChange, "userData" => $userData]);
     }
 
     public function acceptEditChange(Request $request, $userId){
