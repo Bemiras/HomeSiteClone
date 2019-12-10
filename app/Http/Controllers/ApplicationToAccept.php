b50235051728e82c53d62d8df935afac298a8836
Tudi <xickero@gmai.com>
2019-12-10 01:45:41 +0100

    Ten if kurwa ma byc i chuj

--- app/Http/Controllers/ApplicationToAccept.php
+++ app/Http/Controllers/ApplicationToAccept.php
@@ -14,17 +14,38 @@ class applicationToAccept extends Controller
         if(!Auth::check())
             return redirect('/login');
         else {
+
+            $applicationForChangingData = new ApplicationForChangingData;
+            //KURWA TEN IF JEST POTRZEBNY KAMIL, DLA PRACOWNIKA NIC NAM NIE ZWRÓCI RETURN PONIWEAŻ DIRECTION JEST NULLEM.
+            //DlaStudenta
+            if (!is_null($applicationForChangingData->direcion)) {
+                var_dump($applicationForChangingData->direcion);
+                $dataBase = DB::table('application_for_changing_datas')
+                    ->join('departments', 'departments.id', '=', 'application_for_changing_datas.department')
+                    ->join('directions', 'directions.id', '=', 'application_for_changing_datas.direction')
+                    ->join('users', 'users.id', '=', 'application_for_changing_datas.user_id')
+                    ->join('departments AS d', 'd.id', '=', 'users.department')
+                    ->join('directions AS di', 'di.id', '=', 'users.direction')
+                    ->select('application_for_changing_datas.*', 'departments.name AS name_department', 'directions.name AS name_direction',
+                        'users.id AS id_user_users', 'users.name AS old_name', 'users.lastname AS old_lastname', 'users.typestudy AS old_typestudy',
+                        'users.levelstudy AS old_levelstudy', 'd.name AS old_department', 'di.name AS old_direction',
+                        'users.specialization AS old_specialization', 'users.role AS role', 'users.email AS old_email')
+                    ->get();
+                return view('ApplicationToAccept', ["dataBase" => $dataBase]);
+            }//Tu kurwa jest pracownik z DIRECTION = NULL,
+            else{
                 $dataBase = DB::table('application_for_changing_datas')
                     ->join('departments', 'departments.id', '=', 'application_for_changing_datas.department')
                     ->join('users', 'users.id', '=', 'application_for_changing_datas.user_id')
                     ->join('departments AS d', 'd.id', '=', 'users.department')
-                    ->select('application_for_changing_datas.*', 'departments.name AS name_department', 'users.direction AS name_direction',
+                    ->select('application_for_changing_datas.*', 'departments.name AS name_department','application_for_changing_datas.direction AS name_direction',
                         'users.id AS id_user_users', 'users.name AS old_name', 'users.lastname AS old_lastname', 'users.typestudy AS old_typestudy',
-                        'users.levelstudy AS old_levelstudy', 'd.name AS old_department', 'users.direction AS old_direction',
-                        'users.specialization AS old_specialization', 'users.role AS role','users.email AS old_email')
+                        'users.levelstudy AS old_levelstudy', 'd.name AS old_department', 'application_for_changing_datas.direction As old_direction',
+                        'users.specialization AS old_specialization', 'users.role AS role', 'users.email AS old_email')
                     ->get();
-
                 return view('ApplicationToAccept', ["dataBase" => $dataBase]);
+            }
+
 
         }
     }
@@ -33,7 +54,6 @@ class applicationToAccept extends Controller
 
         $userUpdateData = User::where('id', $userId)->first();
         $applicationUpdateData = ApplicationForChangingData::where('user_id', $userId)->first();
-
 
         $userUpdateData->fill([
             'name' => $applicationUpdateData->name,
